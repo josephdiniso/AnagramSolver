392be155b4dfb794a46ff59757960c2d97be52f0
Joseph DiNiso <josephdiniso@gmail.com>
2020-07-21 19:59:02 -0400

    Added webcam support

--- test_face.py
+++ test_face.py
@@ -1,10 +1,9 @@
+import time
+
 import dlib
 from scipy.spatial import distance as dist
 import cv2
 from imutils import face_utils
-
-
-
 
 
 
@@ -26,35 +25,54 @@ def eye_aspect_ratio(eye):
 
 
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("/home/jdiniso/github/AnagramsCheat/shape_predictor_68_face_landmarks.dat")
+predictor = dlib.shape_predictor("/home/josephdiniso/github/AnagramsCheat/shape_predictor_68_face_landmarks.dat")
 
 
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+reset = True
+cap = cv2.VideoCapture(0)
+time_start = 0
+blinks = 0
+while 1:
+    _, frame = cap.read()
+    frame = cv2.resize(frame, (1000,1000))
 
-img = cv2.imread("/home/jdiniso/person.jpg")
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-rects = detector(img, 0)
+    img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    rects = detector(img, 0)
 
-for rect in rects:
-    # determine the facial landmarks for the face region, then
-    # convert the facial landmark (x, y)-coordinates to a NumPy
-    # array
-    frame = cv2.imread("/home/jdiniso/person.jpg")
-    shape = predictor(img, rect)
-    shape = face_utils.shape_to_np(shape)
+    for rect in rects:
+        # determine the facial landmarks for the face region, then
+        # convert the facial landmark (x, y)-coordinates to a NumPy
+        # array
+        shape = predictor(img, rect)
+        shape = face_utils.shape_to_np(shape)
 
-    leftEye = shape[lStart:lEnd]
-    rightEye = shape[rStart:rEnd]
-    leftEAR = eye_aspect_ratio(leftEye)
-    rightEAR = eye_aspect_ratio(rightEye)
-    ear = (leftEAR+rightEAR) / 2
+        leftEye = shape[lStart:lEnd]
+        rightEye = shape[rStart:rEnd]
+        leftEAR = eye_aspect_ratio(leftEye)
+        rightEAR = eye_aspect_ratio(rightEye)
+        ear = (leftEAR+rightEAR) / 2
 
-    leftEyeHull = cv2.convexHull(leftEye)
-    rightEyeHull = cv2.convexHull(rightEye)
-    cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-    cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
-    
+        if ear < 0.34:
+            if not time_start:
+                time_start = time.time() * 1000.
+            if time.time() * 1000. - time_start > 5 and reset:
+                blinks += 1
+                reset = False
+        else:
+            reset = True
+            time_start = None
+
+
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+        cv2.putText(frame,str(blinks),(80,80), cv2.FONT_HERSHEY_SIMPLEX, 3, (255,0,0),2, cv2.LINE_AA) 
+        
     cv2.imshow("img",frame)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
